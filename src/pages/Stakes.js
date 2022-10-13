d0a0beff7076e9703384a0cb6073d1270119da48
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-10-13 20:54:07 +0800

    Update Stakes.js

--- src/pages/Stakes.js
+++ src/pages/Stakes.js
@@ -1,22 +1,110 @@
-import { useContext } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Fragment, useCallback, useContext, useEffect, useState } from "react";
+import { Col, Container, Row, Spinner } from "react-bootstrap";
 import { Navigate } from "react-router-dom";
+import StakeCards from "../components/Cards/StakeCards";
+import WithdrawalCards from "../components/Cards/WithdrawalCards";
 import UserContext from "../store/user-context";
 
 const Stakes = () => {
   const userCtx = useContext(UserContext);
-  if(!userCtx.address) {
-    return <Navigate to='/' replace />;
+  const { address, vault } = userCtx;
+  const [stakeArr, setStakeArr] = useState([]);
+  const [withdrawArr, setWithdrawArr] = useState([]);
+  const [showSpinner, setShowSpinner] = useState(true);
+
+  const getStakeWithdrawArr = useCallback(async () => {
+    const stakeArrIds = await vault.addressToStakeArr(address);
+    const withdrawArrIds = await vault.addressToWithdrawArr(address);
+
+    let userStakeArr = [];
+    let userWithdrawArr = [];
+
+    for (let i = 0; i < stakeArrIds.length; i++) {
+      const stake = await vault.stakes(stakeArrIds[i] - 1);
+      userStakeArr.push(stake);
+    }
+
+    setStakeArr(userStakeArr);
+
+    for (let j = 0; j < withdrawArrIds.length; j++) {
+      const stake = await vault.withdrawals(withdrawArrIds[j] - 1);
+      userWithdrawArr.push(stake);
+    }
+    setWithdrawArr(userWithdrawArr);
+    setShowSpinner(false);
+  }, [address, vault]);
+
+  useEffect(() => {
+    if (!vault) {
+      return;
+    }
+    console.log("useEffect re-render!");
+    getStakeWithdrawArr();
+  }, [address, vault, getStakeWithdrawArr]);
+
+  const unstakeClickHandler = async (stakeId) => {
+    setShowSpinner(true);
+    try {
+      const txn = await vault.submitWithdrawal(stakeId);
+      const receipt = await txn.wait();
+      if (receipt.status === 1) {
+        console.log("Submit Withdrawal is Successful!");
+        await getStakeWithdrawArr();
+      } else {
+        throw new Error("Submit withdrawal failed!");
+      }
+    } catch (error) {
+      console.log("Error: ", error.message);
+      setShowSpinner(false);
+    }
+  };
+
+  const withdrawClickHandler = async (withdrawId) => {
+    setShowSpinner(true);
+    try {
+      const txn = await vault.withdraw(withdrawId);
+      const receipt = await txn.wait();
+      if (receipt.status === 1) {
+        console.log("Withdrawal is Successful!");
+        await getStakeWithdrawArr();
+      } else {
+        setShowSpinner(false);
+        throw new Error("Withdrawal failed!");
+      }
+    } catch (error) {
+      console.log("Error: ", error.message);
+      setShowSpinner(false);
+    }
+  };
+
+  // Redirect if not connected to Metamask or vault contract is not defined
+  if (!address || !vault) {
+    return <Navigate to="/" replace />;
   }
 
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h1>This is Stakes Page</h1>
-        </Col>
-      </Row>
-    </Container>
+    <Fragment>
+      {showSpinner ? (<Spinner animation="border" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>) : null }
+      <Container>
+        <Row className="my-3">
+          <Col>
+            <h1>Stakes</h1>
+          </Col>
+          <StakeCards items={stakeArr} onUnstake={unstakeClickHandler} />
+        </Row>
+        <Row>
+          <Col className="my-3">
+            <h1>Withdrawals</h1>
+          </Col>
+          <WithdrawalCards
+            items={withdrawArr}
+            onWithdraw={withdrawClickHandler}
+          />
+        </Row>
+      </Container>
+    </Fragment>
   );
 };
 
