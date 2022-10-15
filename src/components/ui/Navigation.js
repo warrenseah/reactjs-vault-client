43901316bf922294cf5c1180ddd88792cc729cf2
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-10-15 14:19:40 +0800

    Update Navigation.js
    Added home link to brand logo

--- src/components/ui/Navigation.js
+++ src/components/ui/Navigation.js
@@ -1,20 +1,27 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { LinkContainer } from 'react-router-bootstrap';
-
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Navigation() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">BNB-SuperCharger</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand>BNB-SuperCharger</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
-            <LinkContainer to="/stakes"><Nav.Link>Stakes</Nav.Link></LinkContainer>
-            <LinkContainer to="/rewards"><Nav.Link>Rewards</Nav.Link></LinkContainer>
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/stakes">
+              <Nav.Link>Stakes</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/rewards">
+              <Nav.Link>Rewards</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
