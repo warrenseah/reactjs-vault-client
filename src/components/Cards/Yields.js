1cb91b5d669619d5b2d7d965d61e9ec1c2303bad
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-10-17 14:25:35 +0800

    Added yieldStake reward card

---
+++ src/components/Cards/Yields.js
@@ -0,0 +1,56 @@
+import { Row, Col, Card, ListGroup, Button } from "react-bootstrap";
+import { ethers } from "ethers";
+import { convertToDateTime, truncateTo2DC } from "../../lib/utils";
+import tokens from "../../lib/tokens.json";
+
+function Yields(props) {
+  let count = 0;
+  if (!props.items) {
+    return <p>No reward found!</p>;
+  }
+
+  return (
+    <Row md={2} className="g-4">
+      {props.items.map((item, index) => (
+        <Col key={item.stake.id.toString()}>
+          <Card bg="success" text="white">
+            <Card.Header>
+              Reward Card {++count},{" "}
+              {`YieldId: ${item.yield.id.toNumber() + 1}`}
+            </Card.Header>
+            <Card.Body>
+              <Card.Title>Coin: {tokens[0].name}</Card.Title>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item variant="success">
+                Symbol: {tokens[0].symbol}
+              </ListGroup.Item>
+              <ListGroup.Item variant="success">
+                Start: {convertToDateTime(item.yield.sinceTime.toString())}
+              </ListGroup.Item>
+              <ListGroup.Item variant="success">
+                End: {convertToDateTime(item.yield.tillTime.toString())}
+              </ListGroup.Item>
+            </ListGroup>
+            <Card.Body className="text-center">
+              <Card.Text>Claim: {`${truncateTo2DC(ethers.utils.formatEther(item.claimAmt[1]))} ${tokens[0].symbol}`} </Card.Text>
+              <Button
+                onClick={() => {
+                  props.onClaim(
+                    item.stake.id.toNumber() + 1,
+                    item.yield.id.toNumber() + 1
+                  );
+                }}
+                variant="light"
+              >
+                Claim
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+}
+
+export default Yields;
--- src/pages/Rewards.js
+++ src/pages/Rewards.js
@@ -1,21 +1,112 @@
-import { useContext } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Navigate } from "react-router-dom";
+import Yields from "../components/Cards/Yields";
 import UserContext from "../store/user-context";
+
+// const erc20ABI = [
+//   // Read-Only Functions
+//   "function decimals() view returns (uint8)",
+//   "function name() view returns (string)",
+//   "function symbol() view returns (string)",
+//   "function totalSupply() view returns (uint256)",
+// ];
 
 const Rewards = () => {
   const userCtx = useContext(UserContext);
-  if(!userCtx.address) {
-    return <Navigate to='/' replace />;
+  const [yieldStakesArr, setYieldStakesArr] = useState([]);
+  const { address, vault, signer } = userCtx;
+
+
+  const getStakeArr = useCallback(async () => {
+    const stakeArrIds = await vault.addressToStakeArr(address);
+
+    const userStakeArr = [];
+
+    for (let i = 0; i < stakeArrIds.length; i++) {
+      const stake = await vault.stakes(stakeArrIds[i] - 1);
+      userStakeArr.push(stake);
+    }
+
+    return userStakeArr;
+  }, [address, vault]);
+
+  const init = useCallback(async () => {
+    if (!vault) {
+      return;
+    }
+
+    const endingYields = await vault.getEndedYield(); // need to minus 1
+    const yields = [];
+
+    for (let i = 0; i < endingYields.length; i++) {
+      const item = await vault.yields(endingYields[i].toNumber() - 1);
+      yields.push(item);
+    }
+
+    const stakeArr = await getStakeArr();
+
+    const eligibleStakes = [];
+    // Check if user stake cards exists before yield commence
+    for (let j = 0; j < yields.length; j++) {
+      const yieldSinceTime = yields[j].sinceTime.toNumber();
+      const yieldTillTime = yields[j].tillTime.toNumber();
+
+      // iterate with all available stake cards
+      for (let z = 0; z < stakeArr.length; z++) {
+        const stakeSinceTime = stakeArr[z].sinceTime.toNumber();
+        const stakeTillTime = stakeArr[z].tillTime.toNumber();
+        if (stakeSinceTime <= yieldSinceTime && stakeTillTime < yieldTillTime) {
+          // Find claim amount for eligible stakes
+          const claims = await vault.getClaimsFor(
+            stakeArr[z].id.toNumber() + 1,
+            yields[j].id.toNumber() + 1
+          );
+
+          eligibleStakes.push({
+            yield: yields[j],
+            stake: stakeArr[z],
+            claimAmt: claims,
+          });
+        }
+      }
+    }
+    setYieldStakesArr(eligibleStakes);
+  },[vault, getStakeArr]);
+
+  useEffect(() => {
+    init();
+  }, [address, vault, signer, init]);
+
+  if (!userCtx.address) {
+    return <Navigate to="/" replace />;
   }
-  
+
+  const claimHandler = async (stakeId, yieldId) => {
+    try {
+      const txn = await vault.claimYieldTokens(stakeId, yieldId);
+      const receipt = await txn.wait();
+      if (receipt.status === 1) {
+        console.log(
+          `Claim is successful for ${stakeId} with ${yieldId} reward card!`
+        );
+        await init();
+      } else {
+        throw new Error("Claim failed!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Container>
       <Row>
-        <Col>
+        <Col className="my-3">
           <h1>This is Rewards Page</h1>
         </Col>
       </Row>
+      <Yields items={yieldStakesArr} onClaim={claimHandler} />
     </Container>
   );
 };
