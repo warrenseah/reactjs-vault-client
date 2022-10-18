228219c4ef66926eb5e3e9eeba9ebabf27561c84
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-10-18 14:19:36 +0800

    Toastify codes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import Stakes from "./pages/Stakes";
 import Rewards from "./pages/Rewards";
 import NoPage from "./pages/NoPage";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Fragment } from "react";
+
 const App = () => {
+  
   return (
+    <Fragment>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
@@ -15,6 +21,8 @@ const App = () => {
         <Route path='*' element={<NoPage />} />
       </Route>
     </Routes>
+    <ToastContainer />
+    </Fragment>
   );
 };
 
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,17 +1,32 @@
+import {toast} from 'react-toastify';
+
 export const convertToDateTime = (unix) => {
-    const date = new Date(unix * 1000);
-    return date.toISOString();
+  const date = new Date(unix * 1000);
+  return date.toISOString();
 };
 
-export const haveTimePast = unix => {
-    const now = Math.floor(Date.now() / 1000);
-    if(now > unix) {
-        return true;
-    } else {
-        return false;
-    }
+export const haveTimePast = (unix) => {
+  const now = Math.floor(Date.now() / 1000);
+  if (now > unix) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-export const truncateTo2DC = stringNum => {
-    return stringNum.slice(0, (stringNum.indexOf("."))+3); //With 3 exposing the hundredths place
+export const truncateTo2DC = (stringNum) => {
+  return stringNum.slice(0, stringNum.indexOf(".") + 3); //With 3 exposing the hundredths place
+};
+
+export const showToast = (textString) => {
+  toast(textString, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
 };
