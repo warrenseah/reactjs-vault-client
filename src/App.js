56cacd4b507c8f51fce2e277a9d0c9ebab49ca06
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-10-19 12:05:58 +0800

    Update App.js
    implement lazy loading

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
+import React, { Suspense, Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./components/layout/Layout";
-import Stakes from "./pages/Stakes";
-import Rewards from "./pages/Rewards";
-import NoPage from "./pages/NoPage";
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { Fragment } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { Spinner } from "react-bootstrap";
+
+const Stakes = React.lazy(() => import("./pages/Stakes"));
+const Rewards = React.lazy(() => import("./pages/Rewards"));
+const NoPage = React.lazy(() => import("./pages/NoPage"));
 
 const App = () => {
-  
   return (
     <Fragment>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="stakes" element={<Stakes />} />
-        <Route path="rewards" element={<Rewards />} />
-        <Route path='*' element={<NoPage />} />
-      </Route>
-    </Routes>
-    <ToastContainer />
+      <Suspense
+        fallback={
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="stakes" element={<Stakes />} />
+            <Route path="rewards" element={<Rewards />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+        <ToastContainer />
+      </Suspense>
     </Fragment>
   );
 };
