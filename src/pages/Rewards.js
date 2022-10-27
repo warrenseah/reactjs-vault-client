77ffd1bfd35c4e36d10e1f1fd0256af4d8c106bb
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2022-10-27 16:09:28 +0800

    Update Rewards.js

--- src/pages/Rewards.js
+++ src/pages/Rewards.js
@@ -9,21 +9,22 @@ import { Navigate } from "react-router-dom";
 import Yields from "../components/Cards/YieldRewardCards";
 import UserContext from "../store/user-context";
 import { showToast } from "../lib/utils";
-import {ethers} from 'ethers';
+import { ethers } from "ethers";
 
-// const erc20ABI = [
-//   // Read-Only Functions
-//   "function decimals() view returns (uint8)",
-//   "function name() view returns (string)",
-//   "function symbol() view returns (string)",
-//   "function totalSupply() view returns (uint256)",
-// ];
+const erc20ABI = [
+  // Read-Only Functions
+  "function decimals() view returns (uint8)",
+  "function name() view returns (string)",
+  "function symbol() view returns (string)",
+  "function totalSupply() view returns (uint256)",
+];
 
 const Rewards = () => {
   const userCtx = useContext(UserContext);
   const [yieldStakesArr, setYieldStakesArr] = useState([]);
-  const { address, vault, signer } = userCtx;
+  const { address, vault, signer, provider } = userCtx;
   const [showSpinner, setShowSpinner] = useState(true);
+  const [tokenMeta, setTokenMeta] = useState([]);
 
   const getStakeArr = useCallback(async () => {
     const stakeArrIds = await vault.addressToStakeArr(address);
@@ -39,9 +40,16 @@ const Rewards = () => {
   }, [address, vault]);
 
   const init = useCallback(async () => {
-    if (!vault) {
-      return;
-    }
+    const getErc20Meta = async (tokenAddress) => {
+      const erc20 = new ethers.Contract(tokenAddress, erc20ABI, provider);
+      const name = await erc20.name();
+      const symbol = await erc20.symbol();
+      const decimals = await erc20.decimals();
+      setTokenMeta((prev) => [
+        ...prev,
+        { address: tokenAddress, name, symbol, decimals },
+      ]);
+    };
 
     const endingYields = await vault.getEndedYield(); // need to minus 1
     const yields = [];
@@ -76,13 +84,30 @@ const Rewards = () => {
         }
       }
     }
+
+    for (let i = 0; i < eligibleStakes.length; i++) {
+      getErc20Meta(eligibleStakes[i]["yield"]["token"]);
+    }
+
+    // console.log("In Rewards...");
+    // console.log(eligibleStakes);
     setYieldStakesArr(eligibleStakes);
     setShowSpinner(false);
-  }, [vault, getStakeArr]);
+  }, [vault, getStakeArr, provider]);
 
   useEffect(() => {
+    if (!vault || !provider) {
+      return;
+    }
+    
     init();
-    const filterClaimFrom = vault.filters.ClaimedTokens(null,null,null,address,null);
+    const filterClaimFrom = vault.filters.ClaimedTokens(
+      null,
+      null,
+      null,
+      address,
+      null
+    );
     vault.on(
       filterClaimFrom,
       (yieldId, stakeId, token, user, amount, event) => {
@@ -95,7 +120,7 @@ const Rewards = () => {
           event,
         };
 
-        console.log(txnObj);
+        // console.log(txnObj);
         showToast(
           `stakeId: ${txnObj.stakeId + 1} claimed ${
             txnObj.amount
@@ -107,10 +132,10 @@ const Rewards = () => {
 
     // remove eventListener on unmounting
     return () => {
-      console.log("eventListener is removed!");
+      // console.log("eventListener is removed!");
       vault.removeAllListeners();
     };
-  }, [address, vault, signer, init]);
+  }, [address, vault, signer, init, provider]);
 
   if (!userCtx.address) {
     return <Navigate to="/" replace />;
@@ -125,10 +150,7 @@ const Rewards = () => {
         console.log(
           `Claim success! for ${stakeId} with ${yieldId} reward card!`
         );
-        showToast(
-          `Claim success!`,
-          "success"
-        );
+        showToast(`Claim success!`, "success");
         await init();
       } else {
         throw new Error("Claim failed!");
@@ -140,6 +162,7 @@ const Rewards = () => {
     }
   };
 
+  // console.log(tokenMeta);
   return (
     <Container>
       <Row>
@@ -156,7 +179,11 @@ const Rewards = () => {
           ) : null}
         </Col>
       </Row>
-      <Yields items={yieldStakesArr} onClaim={claimHandler} />
+      <Yields
+        items={yieldStakesArr}
+        tokens={tokenMeta}
+        onClaim={claimHandler}
+      />
     </Container>
   );
 };
