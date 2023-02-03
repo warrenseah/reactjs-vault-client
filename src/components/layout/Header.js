79fcfc8f7a529f786e0408df05c2f18516889d91
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2023-02-03 22:59:47 +0800

    Update Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,14 @@
+import Button from "react-bootstrap/Button";
+
 const Header = () => {
   return (
     <header className="py-5">
-      <div className="container px-lg-5">
+      <div className="container">
         <div className="p-4 p-lg-5 bg-light rounded-3 text-center">
           <div className="m-4 m-lg-5">
-            <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
+            <h1 className="display-5 fw-bold">
+              Stake BNB and Grow Your Altcoins!
+            </h1>
             <p className="fs-4">
               Grow Your Altcoins By Staking BNBs into our Staking Pools. Redeem
               Your BNBs and get your BNBs back in a few days. BNB Charger
@@ -12,9 +16,9 @@ const Header = () => {
               fees will be charged on the altcoins farmed when altcoin rewards
               are claimed.
             </p>
-            <a className="btn btn-primary btn-lg" href="#">
+            <Button variant="primary" size="lg" href="#">
               Start Now
-            </a>
+            </Button>
           </div>
         </div>
       </div>
