7cf2048574578aceda3bb173452244ad8fcbafa8
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2023-02-16 20:48:52 +0800

    Create CallToAction.js

---
+++ src/components/CallToAction.js
@@ -0,0 +1,45 @@
+import { Container, Row } from "react-bootstrap";
+
+const CallToAction = (props) => {
+  return (
+    <Container>
+      <Row className="justify-content-lg-center">
+        <aside className="col-lg-10 bg-primary bg-gradient rounded-3 p-4 p-sm-5 mt-5">
+          <div className="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start">
+            <div className="mb-4 mb-xl-0">
+              <div className="fs-3 fw-bold text-white">
+                News update, delivered to you.
+              </div>
+              <div className="text-white-50">
+                Sign up for our newsletter for the latest updates.
+              </div>
+            </div>
+            <div className="ms-xl-4">
+              <div className="input-group mb-2">
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="Email address..."
+                  aria-label="Email address..."
+                  aria-describedby="button-newsletter"
+                />
+                <button
+                  className="btn btn-outline-light"
+                  id="button-newsletter"
+                  type="button"
+                >
+                  Sign up
+                </button>
+              </div>
+              <div className="small text-white-50">
+                We care about privacy, and will never share your data.
+              </div>
+            </div>
+          </div>
+        </aside>
+      </Row>
+    </Container>
+  );
+};
+
+export default CallToAction;
