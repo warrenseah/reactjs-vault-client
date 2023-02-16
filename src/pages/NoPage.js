0e4a72e70cafae4ddee5b05373774b37800bdb30
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2023-02-16 21:47:53 +0800

    Update NoPage.js

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,13 +1,13 @@
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 const NoPage = () => {
   return (
     <Container>
       <Row>
         <Col>
-          <h1>This page is not found!</h1>
+          <h1 className="text-center">This page is not found!</h1>
         </Col>
       </Row>
     </Container>
