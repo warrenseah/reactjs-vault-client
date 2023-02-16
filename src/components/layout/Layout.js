0347bde30153ca8e3079f3885acc9d976bfca8cb
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2023-02-16 20:48:55 +0800

    Update Layout.js

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,12 +2,14 @@ import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
+import CallToAction from "../CallToAction";
 
 const Layout = () => {
   return (
     <Fragment>
       <Navigation />
       <Outlet />
+      <CallToAction />
       <Footer />
     </Fragment>
   );
