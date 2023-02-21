83a5b17a193c3ce6c248b7e21603c4ce7561a8f1
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2023-02-21 15:22:49 +0800

    Update Features.js

--- src/components/Features.js
+++ src/components/Features.js
@@ -9,9 +9,13 @@ const Features = (props) => {
           <div className="col-lg-8">
             <div className="row gx-5 row-cols-1 row-cols-md-2">
               <div className="col mb-5 h-100">
-                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3">
-                  <i className="bi bi-collection"></i>
+                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3 col-sm-2 text-center">
+                  <i
+                    className="bi bi-collection"
+                    style={{ fontSize: "1.8rem" }}
+                  ></i>
                 </div>
+
                 <h2 className="h5">Stake BNB, Farm Many Altcoins</h2>
                 <p className="mb-0">
                   Don't swap your BNB for altcoins. Using BNB Charger program,
@@ -20,8 +24,11 @@ const Features = (props) => {
                 </p>
               </div>
               <div className="col mb-5 h-100">
-                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3">
-                  <i className="bi bi-building"></i>
+                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3 col-sm-2 text-center">
+                  <i
+                    className="bi bi-building"
+                    style={{ fontSize: "1.8rem" }}
+                  ></i>
                 </div>
                 <h2 className="h5">Choose Farming, Don't Spend</h2>
                 <p className="mb-0">
@@ -31,8 +38,11 @@ const Features = (props) => {
                 </p>
               </div>
               <div className="col mb-5 mb-md-0 h-100">
-                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3">
-                  <i className="bi bi-toggles2"></i>
+                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3 col-sm-2 text-center">
+                  <i
+                    className="bi bi-brightness-alt-low"
+                    style={{ fontSize: "1.8rem" }}
+                  ></i>
                 </div>
                 <h2 className="h5">Low Entry Fees</h2>
                 <p className="mb-0">
@@ -42,8 +52,11 @@ const Features = (props) => {
                 </p>
               </div>
               <div className="col h-100">
-                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3">
-                  <i className="bi bi-toggles2"></i>
+                <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3 col-sm-2 text-center">
+                  <i
+                    className="bi bi-toggles2"
+                    style={{ fontSize: "1.8rem" }}
+                  ></i>
                 </div>
                 <h2 className="h5">Profit Sharing Based on Performance</h2>
                 <p className="mb-0">
